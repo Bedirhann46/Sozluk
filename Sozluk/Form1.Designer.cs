3bf8421dfac0dcd686ff0687a41840bc61299f53
bedirhann46 <bedirhannergiz46@gmail.com>
2024-03-19 10:39:56 +0300

    Proje dosyası ekle.

---
+++ Sozluk/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Sozluk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnDegiştir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "İngilizce"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Türkçe"});
+            this.comboBox2.Location = new System.Drawing.Point(589, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 409);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(502, 39);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(286, 409);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(354, 283);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 47);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnDegiştir
+            // 
+            this.btnDegiştir.Location = new System.Drawing.Point(371, 195);
+            this.btnDegiştir.Name = "btnDegiştir";
+            this.btnDegiştir.Size = new System.Drawing.Size(36, 54);
+            this.btnDegiştir.TabIndex = 6;
+            this.btnDegiştir.Text = "->   <-";
+            this.btnDegiştir.UseVisualStyleBackColor = true;
+            this.btnDegiştir.Click += new System.EventHandler(this.btnDegiştir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDegiştir);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Çeviri1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnDegiştir;
+    }
+}
+
---
+++ Sozluk/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sozluk
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> kelimeler = new Dictionary<string, string>();
+        public Form1()
+       {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            foreach(var kelime in kelimeler)
+            {
+                if(kelime.Key==textBox1.Text)
+                {
+                    textBox2.Text = kelime.Value;
+                    break;
+                }
+                else
+                {
+                    textBox2.Text = "kelime bulunamadı";
+                }
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            kelimeler.Add(textBox1.Text, textBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kelimeler.Add("cat", "kedi");
+            kelimeler.Add("car", "araba");
+            kelimeler.Add("dog", "köpek");
+            kelimeler.Add("black", "siyah");
+
+        }
+
+        private void btnDegiştir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+    
